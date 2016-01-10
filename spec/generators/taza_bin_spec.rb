e869cca1150935462a59aeaa978b9d94687f6c16
Oscar Rieken <oscar_rieken@homedepot.com>
2016-01-10 16:18:38 -0500

    removed output from tests running system("taza"), :cake:

--- spec/generators/taza_bin_spec.rb
+++ spec/generators/taza_bin_spec.rb
@@ -2,19 +2,15 @@ require 'spec_helper'
 require 'rubygems'
 require 'fileutils'
 
-describe "Taza bin" do
-
-  it 'runs from the terminal' do
-    output = system("taza")
-    expect(output).to be true
-
+describe 'Taza bin' do
+  context 'running taza in the terminal' do
+    specify { expect { system('taza') }.to output.to_stdout_from_any_process }
   end
 
-  it "should have an executable script" do
-    skip "Not sure this is really necessary, as this is just testing if command line returns anything. It also breaks JRuby."
+  it 'should have an executable script' do
+    skip 'Not sure this is really necessary, as this is just testing if command line returns anything. It also breaks JRuby.'
     path = 'spec/sandbox/generators'
     taza_bin = "#{File.expand_path(File.dirname(__FILE__)+'/../bin/taza')} #{path}"
     expect(system("ruby -c #{taza_bin} > #{null_device}")).to be true
   end
-
 end
